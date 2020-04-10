95b55974bce7893a114d24254ab30f3749d7ab6f
Kevin Thomas <26861054+Roachbones@users.noreply.github.com>
2020-04-09 20:30:10 -0400

    made relay.py work

--- relay.py
+++ relay.py
@@ -4,12 +4,12 @@ GPIO.setmode(GPIO.BCM)
 
 RELAYS = {"f":26, "b":19, "l":13, "r":6}
 
-GPIO.setup(RELAYS.values(), GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(list(RELAYS.values()), GPIO.OUT, initial=GPIO.LOW)
 cleanup=GPIO.cleanup
 
 BUTTONS_OFF = {"f":False, "b":False, "l":False, "r":False}
 
 def update_relays(buttons=BUTTONS_OFF):
-    for k, pin in RELAYS:
+    for k, pin in RELAYS.items():
         GPIO.output(RELAYS[k], not buttons[k])
     
